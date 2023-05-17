670088ca9f16cd6d7b00b32d46e51cc57306fbcc
Vittoria <vittoria.scocco@usi.ch>
2023-05-17 22:28:48 +0200

    Added missing literals and clauses

--- template.py
+++ template.py
@@ -47,57 +47,86 @@ def gvi(name):
 #     return varMap
 
 
-def gen_vars(nodes, edges):
+def gen_vars(nodes, edges, node_num, k):
     # TODO
     varMap = {}
 
-    for edge in edges:
-        n = "edge_" + edge[0] + "_" + edge[1] # TODO: Not relevant
+    for i in range(0, node_num):
+        n = "n_" + str(i + 1)
+        varMap[n] = gvi(n)
+
+    # counter variables
+    for i in range(0, node_num + 1):
+        for j in range(0, k):
+            n = "s_" + str(i) + "_" + str(j)
+            varMap[n] = gvi(n)
+        n = "s_" + str(i) + "_" + str(k)
         varMap[n] = gvi(n)
 
     return varMap
 
 
-def genConstr(nodes, edges, k, vars):
-    import math
+def gen_clauses(nodes, edges, k, vars):
 
-    # TODO
     clauses = []
+
+    node_num = len(nodes)
+    for i in range(0, node_num):
+        for j in range(i + 1, node_num):
+            # print("looking for " +
+            #       "(" + str(nodes[i]) + ", " + str(nodes[j]) + ")")
+            for edge in edges:
+                found = False
+                if nodes[i] in edge and nodes[j] in edge:
+                    # print(
+                    #     "edge " + "(" + str(nodes[i]) + ", " + str(nodes[j]) + ")" + " exists!")
+                    found = True
+                    break
+                # print(
+                #     "edge " + "(" + str(nodes[i]) + ", " + str(nodes[j]) + ")" + " DNE!")
+            if not found:
+                # print("not found")
+                # add condition
+                # if there is no edge between nodes i and j, then node_i and node_j cannot
+                # both be true at the same time
+                var1 = -vars["n_" + str(i + 1)]
+                var2 = -vars["n_" + str(j + 1)]
+                clauses.append([var1, var2])
+
+    # s_0_0 and s_n_k are always true
+    var = vars["s_0_0"]
+    clauses.append([var])
+    var = vars["s_" + str(node_num) + "_" + str(k)]
+    clauses.append([var])
+
+    # s_0_j is false for i <= j <= k
+    for j in range(0, k):
+        var = -vars["s_0_" + str(j + 1)]
+        clauses.append([var])
+
+    # s_i_0 is true for 0 <= i <= n
+    for i in range(0, node_num):
+        var = vars["s_" + str(i + 1) + "_0"]
+        clauses.append([var])
+
+    # s_n_k is true when either:
+    # a. at least k of x1...xn-1 are true, ie, s(n-1, j) is true
+    # b. at least k-1 of x1...xn-1 are true and xn is true, ie, ( xn and s(n-1, k-1) ) is true
+
+    # remember: a v (b & c) == (a v b) & (a v c)
+
+    for i in range(1, node_num + 1):
+        for j in range(1, k + 1):
+            # a = s(i-1, j)
+            a = vars["s_" + str(i - 1) + "_" + str(j)]
+            # b = x_i
+            b = vars["n_" + str(i)]
+            # c = s(i-1, j-1)
+            c = vars["s_" + str(i - 1) + "_" + str(j - 1)]
+            clauses.append([a, b])
+            clauses.append([a, c])
+
     return clauses
-
-
-# def genPigConstr(pigeons, holes, vars):
-
-#     clauses = []
-
-#     # Insert here the code to generate the clauses.  A single clause var_name | var2_name is added as follows
-#     # clauses.append( [   vars["var_name"],  vars["var2_name"]   ])
-
-#     # Cond 1: A pigeon cannot be inside two holes
-#     for pigeon in range(0, pigeons):
-#         for hole in range(0, holes):
-#             for other_hole in range(hole + 1, holes):
-#                 var1 = -vars["inHole_"+str(pigeon)+"_"+str(hole)]
-#                 var2 = -vars["inHole_"+str(pigeon)+"_"+str(other_hole)]
-#                 clauses.append([var1, var2])
-
-#     # Cond 2: Each hole can contain only one pigeon
-#     for hole in range(0, holes):
-#         for pigeon in range(0, pigeons):
-#             for other_pigeon in range(pigeon + 1, pigeons):
-#                 var1 = -vars["inHole_"+str(pigeon)+"_"+str(hole)]
-#                 var2 = -vars["inHole_"+str(other_pigeon)+"_"+str(hole)]
-#                 clauses.append([var1, var2])
-
-#     # Cond 3: Each pigeon must stay inside some hole
-#     # idea from a, b, c, at least one of them must be true
-#     for pigeon in range(0, pigeons):
-#         list = []
-#         for hole in range(0, holes):
-#             list.append(vars["inHole_"+str(pigeon)+"_"+str(hole)])
-#         clauses.append(list)
-
-#     return clauses
 
 
 # A helper function to print the cnf header
@@ -139,11 +168,27 @@ if __name__ == '__main__':
 
     # vars = gen_vars(pigeons, holes)
 
-    vars = gen_vars(nodes, edges)
+    # check input is valid
+    num_nodes = len(nodes)
+    max_num_edges = num_nodes * (num_nodes - 1) / 2
+
+    if k > num_nodes:
+        print("Can't find a " + str(k) + "-clique in a " +
+              str(num_nodes) + "-node graph")
+        sys.exit(1)
+
+    if len(edges) > max_num_edges:
+        print("Graph has more edges than is feasible for its number of nodes.")
+        sys.exit(1)
+
+    vars = gen_vars(nodes, edges, num_nodes, k)
+    for var in vars:
+        print(var, vars[var])
+    # print(vars)
 
     # rules = genPigConstr(pigeons, holes, vars)
 
-    rules = genConstr(nodes, edges, k, vars)
+    rules = gen_clauses(nodes, edges, k, vars)
 
     head = printHeader(len(rules))
     rls = printCnf(rules)

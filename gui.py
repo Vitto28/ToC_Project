077d77178d90343d7ccca1a496f53a77e54012fd
Alen Sugimoto <alensugimoto@gmail.com>
2023-05-29 19:40:21 +0200

    Adding loading indicator

--- gui.py
+++ gui.py
@@ -4,6 +4,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import PySimpleGUI as sg
 import networkx as nx
 import matplotlib
+import threading
 from tui import kClique
 
 def main():
@@ -20,9 +21,9 @@ def main():
         [sg.InputText('1', key='-K-')],
         [sg.Button('Submit')],
         [sg.Text('_' * 80)],
+        [sg.Text('Loading...', key='-LOADING-', visible=False)],
         [sg.Multiline(key='-OUTPUT-', visible=False)],
-        [sg.Canvas(key='-CANVAS-', visible=False)],
-        [sg.Button('Exit')]
+        [sg.Canvas(key='-CANVAS-', visible=False)]
     ]
 
     window = sg.Window('K-Clique', layout, grab_anywhere=False, size=(2000, 2000), finalize=True)
@@ -32,42 +33,53 @@ def main():
     tkcanvas = FigureCanvasTkAgg(fig, window['-CANVAS-'].TKCanvas)
     tkcanvas.get_tk_widget().pack(side='top', fill='both', expand=1)
 
+    def calculate_output(filepath, k):
+        nodes, edges, _, pos_vars, error = kClique(filepath, k)
+        if pos_vars is not None:
+            G = nx.Graph()
+            G.add_nodes_from(nodes)
+            G.add_edges_from(edges)
+            node_color = ['blue'] * len(nodes)
+            nodes_in_clique = []
+            for v in pos_vars:
+                v = v.split('_')
+                if v[0] == "n":
+                    idx = int(v[1]) - 1
+                    nodes_in_clique.append(nodes[idx])
+                    node_color[idx] = 'red'
+            edge_color = list(map(
+                lambda edge: 'red'
+                    if edge[0] in nodes_in_clique and edge[1] in nodes_in_clique
+                    else 'blue',
+                edges,
+            ))
+            fig.clear()
+            nx.draw_networkx(G,
+                pos=nx.circular_layout(G),
+                ax=fig.gca(),
+                node_color=node_color,
+                edge_color=edge_color)
+            fig.canvas.draw()
+        else:
+            window['-OUTPUT-'].update(error)
+        window['-OUTPUT-'].update(visible=pos_vars is None)
+        window['-CANVAS-'].update(visible=pos_vars is not None)
+        window.write_event_value('JOB DONE', None)
+
     while True:
         event, values = window.read()
 
         if event == 'Submit':
+            window['Submit'].update(disabled=True)
+            window['-OUTPUT-'].update(visible=False)
+            window['-CANVAS-'].update(visible=False)
+            window['-LOADING-'].update(visible=True)
             filepath = values['-FILE_PATH-']
             k = values['-K-']
-            nodes, edges, _, pos_vars, error = kClique(filepath, k)
-            if pos_vars is not None:
-                G = nx.Graph()
-                G.add_nodes_from(nodes)
-                G.add_edges_from(edges)
-                node_color = ['blue'] * len(nodes)
-                nodes_in_clique = []
-                for v in pos_vars:
-                    v = v.split('_')
-                    if v[0] == "n":
-                        idx = int(v[1]) - 1
-                        nodes_in_clique.append(nodes[idx])
-                        node_color[idx] = 'red'
-                edge_color = list(map(
-                    lambda edge: 'red'
-                        if edge[0] in nodes_in_clique and edge[1] in nodes_in_clique
-                        else 'blue',
-                    edges,
-                ))
-                fig.clear()
-                nx.draw_networkx(G,
-                    pos=nx.circular_layout(G),
-                    ax=fig.gca(),
-                    node_color=node_color,
-                    edge_color=edge_color)
-                fig.canvas.draw()
-            else:
-                window['-OUTPUT-'].update(error)
-            window['-OUTPUT-'].update(visible=pos_vars is None)
-            window['-CANVAS-'].update(visible=pos_vars is not None)
+            threading.Thread(target=calculate_output, args=(filepath, k), daemon=True).start()
+        elif event == 'JOB DONE':
+            window['Submit'].update(disabled=False)
+            window['-LOADING-'].update(visible=False)
         elif event in ('Exit', None):
             break
 
